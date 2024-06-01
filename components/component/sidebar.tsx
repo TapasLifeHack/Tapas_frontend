e4c8a2d77fbef6a9bb69b663e5dbbaf3160d9ab9
leesiyuan <lee.si.yuan.20@gmail.com>
2024-06-01 11:25:37 +0800

    Update sidebar.tsx

--- components/component/sidebar.tsx
+++ components/component/sidebar.tsx
@@ -41,7 +41,7 @@ export function SidebarNew() {
                             <FileTextIcon className="size-4" />
                             Assessment
                         </Link> */}
-                        <ExternalLink href="https://www.smartnation.gov.sg/initiatives/digital-government-services/">
+                        <ExternalLink href="https://www.scamshield.org.sg/">
                             <div
                                 className="flex items-center gap-3 rounded-lg pl-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
                             >
@@ -49,7 +49,7 @@ export function SidebarNew() {
                                 Services
                             </div>
                         </ExternalLink>
-                        <ExternalLink href="https://www.gobusiness.gov.sg/gov-assist/">
+                        <ExternalLink href="https://www.scamalert.sg/">
                             <div
                                 className="flex items-center gap-3 rounded-lg pl-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
                             >
